92ded6007efc42bf37fabc2eb390c791ed310f14
itsritiksharma <63971797+itsritiksharma@users.noreply.github.com>
2022-10-29 00:07:45 +0530

    Revert "Version 1 files."

--- src/pages/errorPage.js
+++
@@ -1,5 +0,0 @@
-const ErrorPage = () => {
-  return <h1>Error! Page not found</h1>;
-};
-
-export default ErrorPage;
--- src/utils/constants.js
+++
@@ -1,7 +0,0 @@
-export const API_URL = "https://blog-website-api.onrender.com/";
-
-export const HTTP_STATUS = Object.freeze({
-  PENDING: "pending",
-  FULFILLED: "fulfilled",
-  REJECTED: "rejected",
-});
